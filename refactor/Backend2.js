ce215bd11120e733c7b29c128de3d28b05d4421d
uros_delic <uros.delic@quantox.com>
2021-05-31 15:47:01 +0200

    radi post preko inputa

--- refactor/Backend2.js
+++ refactor/Backend2.js
@@ -4,52 +4,50 @@ port = 8080;
 host = "localhost";
 
 const server = http.createServer((request, response) => {
-
-    if (request.url === "/") {
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET");
-        response.setHeader('content-type', 'text/html');
-        fs.createReadStream('index.html').pipe(response)
-
-    }
-    // GET DATA FROM AJAX
-    else if (request.url === "/get-data" && request.method === "GET") {
-        response.setHeader("content-type", "application/json");
-        let message = fs.readFile("message.json", (error, file) => {
-            if (error) {
-                throw error;
-            } else {
-                message = Buffer.from(file);
-                response.write(file);
-                response.end();
-            };
-        });
-    }
-    // POST DATA FROM AJAX
-    else if (request.method === "POST" && request.url === "/post-data") {
-        response.setHeader("content-type", "application/json");
-        let data = [];
-        request
-            .on("data", (chunk) => {
-                data.push(chunk);
-            })
-            .on("end", () => {
-                data = Buffer.concat(data).toString();
-                fs.writeFile("message.json", data, () => { });
-                response.write(data);
-                response.end();
-            });
-    }
-    else {
-        response.statusMessage = 'bad method';
-        response.write('{"message": "error"}');
+  // jedna putanja za jedan resurs
+  if (request.url === "/") {
+    response.setHeader("Access-Control-Allow-Methods", "POST, GET");
+    response.setHeader("content-type", "text/html");
+    fs.createReadStream("index.html").pipe(response);
+  }
+  // GET DATA FROM AJAX
+  else if (request.url === "/get-data" && request.method === "GET") {
+    response.setHeader("content-type", "application/json");
+    let message = fs.readFile("message.json", (error, file) => {
+      if (error) {
+        throw error;
+      } else {
+        message = Buffer.from(file);
+        response.write(file);
         response.end();
-    }
+      }
+    });
+  }
+  // POST DATA FROM AJAX
+  else if (request.method === "POST" && request.url === "/post-data") {
+    response.setHeader("content-type", "application/json");
+    let data = [];
+    request
+      .on("data", chunk => {
+        data.push(chunk);
+      })
+      .on("end", () => {
+        data = Buffer.concat(data).toString();
+        fs.writeFile("message.json", data, () => {});
+        response.write(data);
+        response.end();
+      });
+  } else {
+    response.statusMessage = "bad method";
+    response.write('{"message": "error"}');
+    response.end();
+  }
 });
 
-server.listen(port, host, (error) => {
-    if (error) {
-        console.log("Something went wrong", error);
-    } else {
-        console.log(`Server is runing on http://${host}:${port}`);
-    }
+server.listen(port, host, error => {
+  if (error) {
+    console.log("Something went wrong", error);
+  } else {
+    console.log(`Server is runing on http://${host}:${port}`);
+  }
 });
